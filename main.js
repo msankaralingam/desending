870cb3fac372e009c225c5ef8ec635f5f26378af
Your Name <smile2sasi95@gmail.com>
2023-03-09 20:38:00 +0530

    add

---
+++ main.js
@@ -0,0 +1,42 @@
+var a=54;
+var b=45;
+var c=20;
+if((a>b)&&(a>c))
+{
+	if(b>c)
+	{
+		console.log(a,b,c);
+	}
+}
+else
+{
+	console.log(a,c,b);
+}
+
+if((b>a)&&(b>c))
+{
+	if(c>a)
+	{
+		
+	    console.log(b,c,a);
+	}
+}
+else
+{
+	    console.log(b,a,c);
+}
+
+if((c>a)&&(c>b));
+{
+	if(a>b)
+	{
+		console.log(c,b,a);
+	}
+	else
+	{
+		console.log(c,b,a);
+	}
+}
+
+	
+
